00f3927fab204685c1d2d675862a55989a12e36a
Filip Petkovic <filip.petkovic@gmail.com>
2019-10-10 13:55:00 +0200

    Initial Commit

---
+++ BarChartAnimated/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarChartAnimated
+//
+//  Created by Filip Petkovic on 10/10/19.
+//  Copyright © 2019 Filip Petkovic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
